13d086a1dd15e84e18e267c7f882d3a7707bdfe3
Jeff Tippmann <jeff.tippmann@gmail.com>
2018-07-15 20:39:20 -0600

    Subprocess

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import os
+import subprocess
 
-os.system("wget -O - -q http://10.0.0.109:30003 | egrep --line-buffered 'MSG,1,|MSG,3,|MSG,5,' >> log_python.txt")
+logfile=open('log_python.txt','w')
+ps = subprocess.Popen(('wget','-O','-','-q','http://10.0.0.109:30003'),stdout=subprocess.PIPE)
+output=subprocess.Popen(('egrep','--line-buffered',"'|MSG,1,|MSG,3,|MSG,4,|'"),stdin=ps.stdout,stdout=subprocess.PIPE)
+
+for line in output.stdout:
+    logfile.write(line.decode('ascii'))
+
+output.wait()
+
